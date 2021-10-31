8995aee98c61075c0fa50519b9b763dffc002635
zuf1 <zlubis374@gmail.com>
2021-10-31 19:21:08 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -39,14 +39,6 @@ if [ "${EUID}" -ne 0 ]; then
 		echo "You need to run this script as root"
 		exit 1
 fi
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-MYIP=$(wget -qO- ipinfo.io/ip);
-IZIN=$( curl https://raw.githubusercontent.com/muhamadparizan/kebenaran/main/ipdibenarkan | grep $MYIP )
-echo ""
-rm -f setup.sh
-fi
 echo '============================================='
 echo '          Sila Tunggu sebentar '
 echo 'Process Update & Upgrade Sedang Dijalankan '
