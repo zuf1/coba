5dad5fbbf6c57a31c13266c77800d54d783e65b0
zuf1 <zlubis374@gmail.com>
2021-10-31 16:22:28 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,36 @@
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- icanhazip.com);
+echo "Checking VPS"
+IZIN=$( curl https://raw.githubusercontent.com/Aantonberkentod/AutoScriptSSH/main/ipvps | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${green}Permission Accepted...${NC}"
+else
+echo -e "${green}Permission Accepted...${NC}"
+fi
+mkdir /etc/v2ray
 mkdir /var/lib/premium-script;
+clear
+echo "Masukkan Domain Anda, Jika Anda Tidak Memiliki Domain Klik Enter Dan"
+echo "Ketikkan Perintah addhost setelah proses instalasi Script Selesai"
 read -p "Hostname / Domain: " host
-echo $host > /root/domain
-wget https://raw.githubusercontent.com/irwanmohi/aidan-vpn/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
-wget https://raw.githubusercontent.com/irwanmohi/aidan-vpn/main/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
-wget https://raw.githubusercontent.com/irwanmohi/aidan-vpn/main/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
-wget https://raw.githubusercontent.com/irwanmohi/aidan-vpn/main/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
-wget https://raw.githubusercontent.com/irwanmohi/aidan-vpn/main/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
-wget https://raw.githubusercontent.com/irwanmohi/aidan-vpn/main/ins-vt.sh && chmod +x ins-vt.sh && sed -i -e 's/\r$//' ins-vt.sh && screen -S v2ray ./ins-vt.sh
-wget https://raw.githubusercontent.com/irwanmohi/aidan-vpn/main/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
+echo "$host" >> /var/lib/premium-script/ipvps.conf 
+echo "$host" >> /etc/v2ray/domain
+wget https://raw.githubusercontent.com/Aantonberkentod/AutoScriptSSH/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
+wget https://raw.githubusercontent.com/Aantonberkentod/AutoScriptSSH/main/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
+wget https://raw.githubusercontent.com/Aantonberkentod/AutoScriptSSH/main/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
+wget https://raw.githubusercontent.com/Aantonberkentod/AutoScriptSSH/main/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
+wget https://raw.githubusercontent.com/Aantonberkentod/AutoScriptSSH/main/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
+wget https://raw.githubusercontent.com/Aantonberkentod/AutoScriptSSH/main/ins-vt.sh && chmod +x ins-vt.sh && sed -i -e 's/\r$//' ins-vt.sh && screen -S v2ray ./ins-vt.sh
+wget https://raw.githubusercontent.com/Aantonberkentod/AutoScriptSSH/main/websocket.sh && chmod +x websocket.sh && screen -S websocket ./websocket.sh
+wget https://raw.githubusercontent.com/Aantonberkentod/AutoScriptSSH/main/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
+apt-get install unzip
 rm -f /root/ssh-vpn.sh
 rm -f /root/sstp.sh
 rm -f /root/wg.sh
 rm -f /root/sodosok.sh
+rm -f /root/websocket.sh
 rm -f /root/ssr.sh
 rm -f /root/ins-vt.sh
 rm -f /root/go.sh
@@ -19,41 +38,46 @@ rm -f /root/ipsec.sh
 history -c
 echo "1.1" > /home/ver
 clear
-echo -e " "
-echo -e "\e[1;32mInstallation has been completed!!"
-echo -e "\e[1;32mInstallation has been completed!!"
-echo -e " "
-echo -e "\e[1;33m\e[1;33m\e[1;33m=================================\e[1;36m\e[1;32m-Autoscript Premium-\e[1;33m\e[1;33m===========================" | tee -a log-install.txt
-echo -e "" | tee -a log-install.txt
-echo -e "\e[1;36m   >>> Service & Port"  | tee -a log-install.txt
-echo -e "\e[1;32m  - OpenSSH                 :\e[1;34m\e[1;34m 22"  | tee -a log-install.txt
-echo -e "\e[1;32m  - OpenVPN                 :\e[1;34m TCP 1194, UDP 2200, SSL 992"  | tee -a log-install.txt
-echo -e "\e[1;32m  - Stunnel4                :\e[1;34m 443"  | tee -a log-install.txt
-echo -e "\e[1;32m  - Dropbear                :\e[1;34m 143, 109"  | tee -a log-install.txt
-echo -e "\e[1;32m  - Squid Proxy             :\e[1;34m 3128, 8080 (limit to IP Server)"  | tee -a log-install.txt
-echo -e "\e[1;32m  - Badvpn                  :\e[1;34m 7100, 7200, 7300"  | tee -a log-install.txt
-echo -e "\e[1;32m  - Nginx                   :\e[1;34m 81"  | tee -a log-install.txt
-echo -e "\e[1;32m  - Wireguard               :\e[1;34m 7070"  | tee -a log-install.txt
-echo -e "\e[1;32m  - L2TP/IPSEC VPN          :\e[1;34m 1701"  | tee -a log-install.txt
-echo -e "\e[1;32m  - Shadowsocks-R           :\e[1;34m 1443-1543"  | tee -a log-install.txt
-echo -e "\e[1;32m  - SS-OBFS TLS             :\e[1;34m 2443-2543"  | tee -a log-install.txt
-echo -e "\e[1;32m  - SS-OBFS HTTP            :\e[1;34m 3443-3453"  | tee -a log-install.txt
-echo -e "\e[1;32m  - V2RAY Vmess TLS         :\e[1;34m 4443"  | tee -a log-install.txt
-echo -e "\e[1;32m  - V2RAY Vmess None TLS    :\e[1;34m 80"  | tee -a log-install.txt
-echo -e "\e[1;32m  - Trojan                  :\e[1;34m 6443"  | tee -a log-install.txt
-echo -e ""  | tee -a log-install.txt
-echo -e "\e[1;36m   >>> Server Information & Other Features"  | tee -a log-install.txt
-echo -e "\e[1;32m  - Timezone                :\e[1;34m Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
-echo -e "\e[1;32m  - Fail2Ban                :\e[1;34m [ON]"  | tee -a log-install.txt
-echo -e "\e[1;32m  - Dflate                  :\e[1;34m [ON]"  | tee -a log-install.txt
-echo -e "\e[1;32m  - IPtables                :\e[1;34m [ON]"  | tee -a log-install.txt
-echo -e "\e[1;32m  - Auto-Reboot             :\e[1;34m [ON]"  | tee -a log-install.txt
-echo -e "\e[1;32m  - IPv6                    :\e[1;34m [OFF]"  | tee -a log-install.txt
-echo -e "\e[1;32m  - Autoreboot On 00.00 GMT +7" | tee -a log-install.txt
-echo -e "\e[1;32m  - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
-echo -e ""  | tee -a log-install.txt
-echo -e "\e[1;33m\e[1;33m------------------------------------------\e[1;36m[Badboy]\e[1;33m------------------------------------------" | tee -a log-install.txt
-echo -e ""
+echo " "
+echo "Installation has been completed!!"
+echo " "
+echo "=================================-Autoscript Premium by HableSSH-===========================" | tee -a log-install.txt
+echo "" | tee -a log-install.txt
+echo "--------------------------------------------------------------------------------" | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "   >>> Service & Port"  | tee -a log-install.txt
+echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
+echo "   - OpenVPN                 : TCP 1194, UDP 2200, SSL 992"  | tee -a log-install.txt
+echo "   - Stunnel4                : 443, 222, 777"  | tee -a log-install.txt
+echo "   - Dropbear                : 143, 109"  | tee -a log-install.txt
+echo "   - SSH-WS                  : 2082"  | tee -a log-install.txt
+echo "   - OVPN-WS                 : 2086"  | tee -a log-install.txt
+echo "   - Squid Proxy             : 3128, 8080 (limit to IP Server)"  | tee -a log-install.txt
+echo "   - Badvpn                  : 7100, 7200, 7300"  | tee -a log-install.txt
+echo "   - Nginx                   : 81"  | tee -a log-install.txt
+echo "   - Wireguard               : 7070"  | tee -a log-install.txt
+echo "   - L2TP/IPSEC VPN          : 1701"  | tee -a log-install.txt
+echo "   - PPTP VPN                : 1732"  | tee -a log-install.txt
+echo "   - SSTP VPN                : 5555"  | tee -a log-install.txt
+echo "   - Shadowsocks-R           : 1443-1543"  | tee -a log-install.txt
+echo "   - SS-OBFS TLS             : 2443-2543"  | tee -a log-install.txt
+echo "   - SS-OBFS HTTP            : 3443-3453"  | tee -a log-install.txt
+echo "   - V2RAY Vmess TLS         : 8443"  | tee -a log-install.txt
+echo "   - V2RAY Vmess None TLS    : 80"  | tee -a log-install.txt
+echo "   - Trojan                  : 2087"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
+echo "   - Timezone                : Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
+echo "   - Fail2Ban                : [ON]"  | tee -a log-install.txt
+echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
+echo "   - IPtables                : [ON]"  | tee -a log-install.txt
+echo "   - Auto-Reboot             : [ON]"  | tee -a log-install.txt
+echo "   - IPv6                    : [OFF]"  | tee -a log-install.txt
+echo "   - Autoreboot On 05.00 GMT +7" | tee -a log-install.txt
+echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "------------------------------------------BY HAWENG GANS------------------------------------------" | tee -a log-install.txt
+echo ""
 echo " Reboot 10 Sec"
 sleep 10
 reboot
