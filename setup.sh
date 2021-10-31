b7d66958433f6d7d51ba1965242720f55a6ff6fe
zuf1 <zlubis374@gmail.com>
2021-10-31 10:22:38 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@ mkdir /var/lib/premium-script;
 clear
 echo -e "\e[1;32mEnter the VPS Subdomain Hostname, if not available, please click Enter"
 read -p "Hostname / Domain: " host
-echo "IP=$host" >> /var/lib/premium-script/ipvps.conf
 echo $host > /root/domain
 wget https://raw.githubusercontent.com/irwanmohi/aidan-vpn/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
 wget https://raw.githubusercontent.com/irwanmohi/aidan-vpn/main/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
