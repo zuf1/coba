2614a88dee625794dd0e0f56793b0d61f72ab474
zuf1 <zlubis374@gmail.com>
2021-11-04 21:34:38 +0700

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "# Script Auto Install VPS PREMIUM               #"
+echo -e "#-----------------------------------------------#"
+echo -e "# For Debian 9 & 10 Buster 64 bit               #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "# Modified by ZET TV                            #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Thanks To                                     #"
+echo -e "#-----------------------------------------------#"
+echo -e "# ALLAH SWT                                     #"
+echo -e "# MY YOUTUBE CHANEL                             #"
+echo -e "# "ZET TV"                                      #"
+echo -e "# PREMIUM SCRIPT                                #"
+echo -e "#                                               #"
+echo -e "#-----------------------------------------------#"
+echo -e "================================================="
