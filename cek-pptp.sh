49738f541a0c36eaa80c8195dbec9fdd11ba9f9b
zuf1 <zlubis374@gmail.com>
2021-11-07 17:30:14 +0700

    Update cek-pptp.sh

--- cek-pptp.sh
+++ cek-pptp.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
-IZIN=$( curl http://64.227.12.202/askedvpstech/ipbaikbanget | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${green}Permission Accepted...${NC}"
-else
-echo -e "${red}Permission Denied!${NC}";
-echo "Only For Premium Users"
-exit 0
-fi
-clear
 last | grep ppp | grep still | awk '{print " ",$1," - " $3 }' > /tmp/login-db-pptp.txt;
 echo " "
 echo " "
